d7926bfaf465a279944a5ffa6d42e6238df12fcf
Julen Perez Coca <julper23@gmail.com>
2026-06-07 21:12:28 +0200

    Control props alt

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -1,32 +1,34 @@
-import { useEffect, useRef, useState } from "react"
-import { onChangeArgs, Product } from "../interfaces/interfaces"
+import { useEffect, useState } from 'react'
+import { onChangeArgs, Product } from '../interfaces/interfaces';
+
 
 interface useProductArgs {
-    product:Product,
-    onChange?: (args:onChangeArgs) => void,
-    value?: number
+    product: Product;
+    onChange?: ( args: onChangeArgs ) => void;
+    value?: number;
 }
+
 
 export const useProduct = ({ onChange, product, value = 0 }: useProductArgs) => {
 
-    const [counter,setCounter] = useState(value)
+    const [ counter, setCounter ] = useState( value );
 
-    const isControlled = useRef( !!onChange )
 
-    useEffect(()=>{
-        setCounter(value)
-    },[value])
+    const increaseBy = ( value: number ) => {
+      
+        const newValue = Math.max( counter + value, 0 )
+        setCounter( newValue );
 
-    const increaseBy = (value:number)=>{
-        if(isControlled.current){
-            return onChange && onChange({count:value, product})
-        }
-        const newValue = Math.max(counter + value,0)
-        setCounter(newValue)
+        onChange && onChange({ count: newValue, product });
     }
+
+    useEffect(() => {
+        setCounter( value );
+    }, [ value ])
 
     return {
         counter,
         increaseBy
     }
+
 }
--- src/02-component-patterns/hooks/useShoppingCart.ts
+++ src/02-component-patterns/hooks/useShoppingCart.ts
@@ -1,34 +1,34 @@
-import { useState } from "react";
-import { Product, ProductInCart } from "../interfaces/interfaces";
+import { useState } from 'react';
+import { Product, ProductInCart } from '../interfaces/interfaces';
+
+
+
 export const useShoppingCart = () => {
 
-    const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCart }>({})
-  
-    const onProductCountChange = ({count, product} : {count:number, product:Product}) => {
+    const [ shoppingCart, setShoppingCart ] = useState<{ [key:string]: ProductInCart  }>({});
+
+    const onProductCountChange = ({ count, product }: { count:number, product: Product }) => {
+        
+        console.log({ count })
+
         setShoppingCart( oldShoppingCart => {
-              
-        const productInCart: ProductInCart = oldShoppingCart[product.id] || {...product, count: 0}
-              
-        const updatedProduct = {
-            ...productInCart,
-            count: productInCart.count + count
-        }
-  
-        if(Math.max(productInCart.count + count, 0)>0){
-            productInCart.count =  productInCart.count + count
+
+            if( count === 0 ) {
+                const {  [product.id]: toDelete, ...rest  } = oldShoppingCart;
+                return rest;
+            }
+
             return {
                 ...oldShoppingCart,
-                [product.id] : updatedProduct
-            }
-            }else{
-                const { [product.id]: toDelete, ...rest} = oldShoppingCart
-                return {...rest}
+                [ product.id ]: { ...product, count }
             }
         })
+
     }
 
     return {
         shoppingCart,
-        onProductCountChange
+        onProductCountChange,
     }
+
 }
